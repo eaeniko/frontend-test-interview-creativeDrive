dc7ae1a2a396b4c7f5cca279a40c8cc0b41637bc
eaeniko <eaenikolai@gmail.com>
2021-09-30 12:52:32 -0300

    Init project, base files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
+import User from './pages/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/admin" component={Admin} />
+        <Route exact path="/user" component={User} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
